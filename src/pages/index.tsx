8f911400d1af2ec498e48c54da892a297bd7d4aa
Mehmet Sağır <mehmetsagir@Mehmet-MacBook-Air.local>
2021-11-14 22:23:32 +0300

    add: followers page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from 'next';
+import UserList from 'components/common/UserList';
+import useFollowers from 'src/hooks/useFollowers';
+import PageLoader from 'components/Loaders/PageLoader';
+import { useUser } from 'src/hooks/useUser';
 
 const Home: NextPage = () => {
-  return <div>Followers</div>;
+  const { user } = useUser();
+  const { followers, isLoading } = useFollowers();
+
+  if (isLoading)
+    return (
+      <PageLoader count={user.followers.length > 12 ? 12 : user.folllowers} />
+    );
+  if (!followers) return null;
+
+  return <UserList data={followers} />;
 };
 
 export default Home;
