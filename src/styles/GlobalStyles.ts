af62797515d733e9f7d8647cba56fabc6d2f45db
Mehmet Sağır <mehmetsagir@Mehmet-MacBook-Air.local>
2021-11-15 13:01:00 +0300

    fix: layout-loader

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -57,47 +57,6 @@ const GlobalStyle = createGlobalStyle`
       background: var(--color-primary-hover);
     }
 	}
-	.layout-loader {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		flex-direction: column;
-		height: 100vh;
-
-		svg {
-			width: 120px;
-			height: 120px;
-			fill: rgba(255, 255, 255, 0.3);
-			animation: opacity 500ms ease-in-out forwards;
-		}
-		span {
-			display: block;
-			margin-top: 25px;
-			color: rgba(255, 255, 255, 0.3);
-			font-size: 28px;
-			font-weight: bolder;
-			animation: opacity 500ms ease-in-out forwards;
-		}
-
-		@keyframes opacity {
-			from {
-				opacity: 0;
-			}
-			to {
-				opacity: 1;
-			}
-		}
-
-		@media (max-width: 500px) {
-			svg {
-				width: 80px;
-				height: 80px;
-			}
-			span {
-				font-size: 20px;
-			}
-		}
-	}
 `;
 
 export default GlobalStyle;
